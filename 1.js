6a0adfe1e6530f17a9b7c5972802f3ddee9a9a30
ak171297 <31471806+ak171297@users.noreply.github.com>
2018-06-30 15:04:53 +0530

    new

--- 1.js
+++ 1.js
@@ -1,5 +1,8 @@
 const express = require('express') 
 var fs = require('fs')
+var sa = require('superagent');
+var request = require('request');
+var stringSimilarity = require('string-similarity');
 //const logger = require('morgan')
 //const errorhandler = require('errorhandler')
 const mongodb= require('mongodb')
@@ -12,6 +15,8 @@ const url = 'mongodb://localhost:27017/mydb'
 let app = express()
 var counts = 0
 var arr = []
+var score = []
+var sizechk = 0
 //app.use(express.static('public'));
 //app.use(logger('dev'))
 //app.use(bodyParser.json())
@@ -24,87 +29,87 @@ MongoClient.connect(url, (err, client) => {
 
   app.get('/UI.htm', function (req, res) {
     res.sendFile( __dirname + "/" + "UI.htm" );
- })
+  })
   app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
-  Userresponse = {
-     filename:req.query.filename,
-     chk:req.query.chk
-  };
-  console.log(Userresponse);
-  res.end(JSON.stringify(Userresponse));
-
+    // Prepare output in JSON format
+    const Userresponse = {
+      filename: req.query.filename,
+      chk: req.query.chk
+    };
+    console.log(Userresponse);
     var reader = csv.createCsvFileReader(Userresponse.filename, { columnsFromHeader: true }); 
-reader.addListener('data', function(data) { db.collection('books').insertOne(data,{'filename':Userresponse.filename},function(err, result) {
-    if(!!err){
-        return res.end("Error uploading files.", err);
-    }else{
-        res.end("File is uploaded");
+    reader.addListener('data', function(data) {
+      data.filename = Userresponse.filename;
+      //console.log(data);
+      db.collection('books')
+        .insertOne(data, function(err, result) {
+        })
+    })
+    async function run(){
+    reader.addListener('end', function() {
+      db.collection('books').find("Number1").count()
+      .then(async function (counts) {
+        res.status(200).json(counts);
+        sizechk = await call(counts)
+       
+        function call(forchk){
+         var a =  (forchk*Userresponse.chk)/100
+          return a
+           }
+  
+  
+    await db.collection('books').aggregate([{ $sample: { size: sizechk } }
+    ]).toArray(async function(err, result) {
+      if (err) throw err;
+      console.log(sizechk)
+      
+       for(var i=0;i<sizechk;i++)
+        {
+      await Promise.all([mainfunct(i),console.log(i),timeout(5000)])
+        }
+        function timeout(ms) {
+          return new Promise(resolve => setTimeout(resolve, ms));
+      }
+        async function  mainfunct(a){ 
+           
+        //console.log(result[a].Number1)
+        // console.log(result[a].Name)
+        await request.post(
+          'https://mnz6d8xba6.execute-api.ap-south-1.amazonaws.com/production/api/v1/search',
+          { json: { phone: result[a].Number1 } },
+          async function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                await console.log(body.result.data[0].name)
+                await console.log(result[a].Name)
+            
+                var str1 = await body.result.data[0].name
+                var patt = await new RegExp(/result[a].Name/ig)
+                var resa = await patt.test(str1);
+                console.log(resa)
+                      //if (resa) {
+                        //await console.log(body.result.data[0].name)
+                        //await console.log(result[a].Name)
+                        //await console.log(true);
+              //        //  score[a] = 100
+                    //} else {
+                        //await console.log(false);
+                       // await console.log(body.result.data[0].name)
+                  //await console.log(result[a].Name)
+              //          //score[a] = 0
+                    //}
+              //      //var fscore = fscore+score[i];
+              //     // var average = fscore/i;
+              //     // res.status(200).json(average)                             
+              }
+          }
+      );
     }
     });
-    })
-    //to add file name(but not adding)
-    db.collection('books').update({},
-      {$push : {"Filename":Userresponse.filename}},
-      {upsert:false,
-      multi:true}) 
-      .then(function(updateddata){console.log('data updated');})
-      //to count the number of doc(but returning the value before upload)
-db.collection('books').find("Number1").count()
-    .then(function(counts) {
-      console.log(counts); 
-      
-    })
-   arr =  db.collection('books')
-      .find({}, { "Number1": 1, _id:0 })
-      
-
-
+  })
 })
-
-  // csv.fromPath(Userresponse.filename)
-  //   .on('data', function(data) {
-
-  //     // `data` is an array containing the values
-  //     // of the current line in the file
-    
-  //     var myobj = data;
-  //     var dbo = client.db("mydb");
-  //     //var myobj = JSON.stringify(data);
-  //     dbo.collection("customers").insertOne(myobj, function(err, res) {
-  //       if (err) throw err;
-  //       console.log("1 document inserted");})
-  //     console.log(myobj);
-
-  //     })
-  //   .on('end', function() {
-  //     console.log('Parsing complete!')
-      
-
-  //   })
-    
-   
- 
-
-app.get('/mydb', (req, res) => {
-  
-    
-    res.send(util.inspect(db.collection('books')
-    .find({}, { "Number1": 1, _id:0 })))
-  
-  // db.collection('books')
-  //     .find({}, { "Number1": 1, _id:0 })
-  //     .toArray((error, number) => {
-  //       client.close();
-  //       if (error) return next(error)
-  //       res.send(number)
-      
-  //     });
-  //     console.log(arr)
+}
+run();
   })
   app.listen(3000)
-  // var num = db.ajay.find().toArray();
-  //   (error, number) => {
-    //     // if (error) return next(error)
-        // console.log(number)})
+
 })
